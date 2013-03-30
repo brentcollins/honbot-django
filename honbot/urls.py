b06494dea14b7aaef7a12a77e29317fc9a5305d6
Scott Cooper <scttcper@gmail.com>
2013-03-30 14:11:36 -0700

    add robots.txt

--- honbot/urls.py
+++ honbot/urls.py
@@ -1,6 +1,7 @@
 # app specific urls
 from django.conf.urls import patterns, include, url
 from django.conf import settings
+from django.views.generic import TemplateView
 
 
 urlpatterns = patterns('',
@@ -8,4 +9,5 @@ urlpatterns = patterns('',
     url(r'^player/(?P<name>.*)/$', 'honbot.views.players'),
     url(r'^match/(?P<match_id>\d+)/$', 'honbot.views.match_view'),
     url(r'^chat/(?P<match_id>\d+)/$', 'honbot.views.chat_view'),
+    (r'^robots\.txt$', TemplateView.as_view(template_name="robots.txt")),
 )
